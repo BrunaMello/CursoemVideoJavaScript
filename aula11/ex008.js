c917afa07d58e7d720062ab44faaee003f3dd900
brunamello <brunacrespomello@gmail.com>
2019-06-14 13:12:03 +0100

    Commit 2 dia 14

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro e ${vel} por hora`)
+if (vel >= 61.0) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')}
+else {
+    console.log('Dirija sempre usando o cinto de segunraca!')}
